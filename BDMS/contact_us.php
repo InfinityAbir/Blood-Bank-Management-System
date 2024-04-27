1b1dccc7b660423fb8c4c3a279f542d92dae6859
Abri Hasan <154973446+InfinityAbir@users.noreply.github.com>
2024-04-27 14:47:52 +0600

    Update contact_us.php

--- BDMS/contact_us.php
+++ BDMS/contact_us.php
@@ -19,7 +19,7 @@ if(isset($_POST["send"])){
 $number=$_POST['contactno'];
 $email=$_POST['email'];
 $message=$_POST['message'];
-$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
+$conn=mysqli_connect("localhost","root","","blood_bank_database") or die("Connection error");
 $sql= "insert into contact_query (query_name,query_mail,query_number,query_message) values('{$name}','{$number}','{$email}','{$message}')";
 $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
   echo '<div class="alert alert-success alert_dismissible"><b><button type="button" class="close" data-dismiss="alert">&times;</button></b><b>Query Sent, We will contact you shortly. </b></div>';
